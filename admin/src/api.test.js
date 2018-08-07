c60dfb809365a32d0b332664a2eef70071c86587
fain182 <fain182@gmail.com>
2018-08-08 00:35:06 +0200

    fix typo

--- admin/src/api.test.js
+++ admin/src/api.test.js
@@ -4,10 +4,10 @@ test("creates a metric", () => {
   expect.assertions(1);
   var expected = {
     name: "pippo",
-    url: "http://ww.google.it",
+    url: "http://www.google.it",
     selector: "#my_id"
   };
   return expect(
-    createMetricAPI("pippo", "http://ww.google.it", "#my_id")
+    createMetricAPI("pippo", "http://www.google.it", "#my_id")
   ).resolves.toEqual(expected);
 });
