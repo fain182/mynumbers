172b0389a1a988c8615793f418ac816190c0d2c4
fain182 <fain182@gmail.com>
2018-08-10 11:09:09 +0200

    Improve validation error UI

--- admin/src/MetricForm.js
+++ admin/src/MetricForm.js
@@ -8,7 +8,7 @@ class MetricForm extends Component {
     this.urlInput = React.createRef();
     this.selectorInput = React.createRef();
 
-    this.state = { message: "" };
+    this.state = { validationError: "", successMessage: "" };
   }
 
   handleSubmit(event) {
@@ -20,64 +20,77 @@ class MetricForm extends Component {
       this.selectorInput.current.value
     )
       .then(result => {
-        this.setState({ message: "'" + result.name + "' has been added." });
+        this.setState({
+          successMessage: "'" + result.name + "' has been added.",
+          validationError: ""
+        });
       })
       .catch(error => {
-        this.setState({ message: error.message });
+        this.setState({ validationError: error.message });
       });
   }
 
   render() {
-    if (this.state.message.length > 0) {
+    if (this.state.successMessage.length > 0) {
       return (
         <div className="row">
           <div className="col-md-6 offset-md-3">
-            <h1>{this.state.message}</h1>
+            <h1>{this.state.successMessage}</h1>
           </div>
         </div>
       );
     }
 
     return (
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <form onSubmit={e => this.handleSubmit(e)}>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input
-                ref={this.nameInput}
-                type="text"
-                required
-                className="form-control"
-                id="name"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="url">Url</label>
-              <input
-                ref={this.urlInput}
-                type="text"
-                required
-                className="form-control"
-                id="url"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="selector">CSS selector</label>
-              <input
-                ref={this.selectorInput}
-                type="text"
-                required
-                className="form-control"
-                id="selector"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Track this number!
-            </button>
-          </form>
+      <React.Fragment>
+        <div className="row">
+          <div className="col-md-6 offset-md-3">
+            <form onSubmit={e => this.handleSubmit(e)}>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  ref={this.nameInput}
+                  type="text"
+                  required
+                  placeholder="Facebook likes of my page"
+                  className="form-control"
+                  id="name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="url">Url</label>
+                <input
+                  ref={this.urlInput}
+                  type="text"
+                  placeholder="https://www.facebook.com/mypage"
+                  required
+                  className="form-control"
+                  id="url"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="selector">CSS selector</label>
+                <input
+                  ref={this.selectorInput}
+                  type="text"
+                  required
+                  placeholder="#abc"
+                  className="form-control"
+                  id="selector"
+                />
+              </div>
+              {this.state.validationError.length > 0 ? (
+                <div class="alert alert-danger" role="alert">
+                  {this.state.validationError}
+                </div>
+              ) : null}
+              <button type="submit" className="btn btn-primary">
+                Track this number!
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
