d44006967783f6ac3ce3bbf73dca223d10b37864
fain182 <fain182@gmail.com>
2018-08-19 20:42:30 +0200

    show metric list in homepage

--- graphs/pages/index.js
+++ graphs/pages/index.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
 
-const Index = () => (
+const Index = props => (
   <Layout>
     <h2>Homepage</h2>
+    <ul>
+      {props.metrics.map(metric => {
+        return <li>{metric.name}</li>;
+      })}
+    </ul>
   </Layout>
 );
+
+Index.getInitialProps = async ({ req }) => {
+  const res = await fetch("http://127.0.0.1:8000/metrics");
+  const json = await res.json();
+
+  return { metrics: json };
+};
 
 export default Index;
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ./api/bin/console server:run &
-cd admin && yarn start
+cd admin && yarn run start &
+cd graphs && yarn run dev 
 
